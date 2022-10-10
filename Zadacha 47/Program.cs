53a0e5f7bc62a5d21d0b32fd51bf6cc96ebf571a
Anton <pavlov@mail.ru>
2022-10-10 19:47:25 +0500

    Zadacha 47

---
+++ Zadacha 47/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+//получает число с консоли
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+double[,] InitMatrix(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * 100 - 50, 2);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите число m");
+int n = GetNumber("Введите число n");
+double[,] matrix = InitMatrix(m, n);
+
+Console.WriteLine("Матрица:");
+PrintMatrix(matrix);
